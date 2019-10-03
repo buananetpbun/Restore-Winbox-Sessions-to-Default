35c6e00548a1553030d2f53fe540ecc837490a0a
Agus Ramadhani <42666125+buananetpbun@users.noreply.github.com>
2019-10-03 08:28:41 +0700

    Add files via upload

---
+++ winbox-sessions-restore.bat
@@ -0,0 +1,38 @@
+@echo OFF
+:: Window Attributes  
+TITLE Restore Winbox Sessions to Default
+COLOR 1F  
+mode CON: cols=51 lines=30
+echo.  
+echo  ‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹‹
+echo  ›                                               ›
+echo  › Restore Winbox Sessions to Default            ›
+echo  › By BuanaNET SECURE, Okt 2019                  ›
+echo  › fb.com/buananet.pangkalanbun                  ›
+echo  ›                                               ›
+echo  ﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂﬂ
+echo.  
+:choice
+set /P c=Restore Winbox Session[Y/N]?
+if /I "%c%" EQU "Y" goto :winbox-yes
+if /I "%c%" EQU "N" goto :winbox-no
+goto :choice
+:winbox-yes
+echo.
+echo Close Winbox.exe Proccess..
+echo -------------------------------------------------
+taskkill /f /im winbox.exe
+echo -------------------------------------------------
+echo.
+echo Remove all sessions winbox at file *.viw
+echo -------------------------------------------------
+echo Location: c:\Users\%USERNAME%\AppData\Roaming\Mikrotik\Winbox\sessions\
+DEL "c:\Users\%USERNAME%\AppData\Roaming\Mikrotik\Winbox\sessions\*.viw"
+echo -------------------------------------------------
+echo. 
+echo ***** Process Complete! *****
+echo. 
+pause 
+exit
+:winbox-no
+exit
